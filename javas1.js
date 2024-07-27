bebc70516882ccb70ed6e9882cdb840413279316
Ishwarya1219 <ishwaryasri1912@gmail.com>
2024-07-27 22:36:47 +0530

    operators

--- javas1.js
+++ javas1.js
@@ -1,1 +1,64 @@
-console.log("Hello world");
+// Arithmetic operators:
+
+var a=32;
+var b=14;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+
+//Assignment operators
+
+let c=10;
+console.log(c);
+c+=5;
+console.log(c);
+c-=5;
+console.log(c);
+c*=5;
+console.log(c);
+c/=5;
+console.log(c);
+c%=5;
+console.log(c);
+
+
+//Comparsion operators
+
+let d=9;
+console.log(d==9);
+console.log(d!=9);
+console.log(d===9);
+console.log(d!==9);
+console.log(d>9);
+console.log(d>=9);
+console.log(d<9);
+console.log(d<=9);
+
+
+
+// Logical operators
+
+console.log(4>3&&2>1);
+console.log(4<3&&2>1);
+
+console.log(4<3||2>1);
+console.log(4<3||2>1);
+
+console.log(!true);
+console.log(!false);
+
+//Ternary operator
+
+console.log(2==='2')? 'x':'y';
+console.log(2===2)? 'x':'y';
+
+
+//typeof Operator
+
+console.log(typeof true);
+console.log(typeof 2);
+console.log(typeof '5');
+console.log(typeof 5.4);
